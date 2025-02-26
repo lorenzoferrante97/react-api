4dd6d71af7a4abcff41d65e2e561ea0fae656984
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-26 13:07:33 +0100

    add: table with api result

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,9 @@ function App() {
 
   useEffect( () => {
 
-    fetch('localhost:3000/posts/')
+    fetch('http://localhost:3000/posts/')
     .then(response => response.json())
-    .then( data => {
-
-      setPosts(data)
-
-    })
+    .then( (data) => { setPosts(data) })
     .catch(error => {
 
       console.error(error);
@@ -26,7 +22,41 @@ function App() {
 
     <>
       <div className="min-h-screen w-full flex flex-col gap-4u items-center py-12u">
-        
+        {/* tabella */}
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Content</th>
+              <th>Image</th>
+              <th>Tags</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              posts.map( (post) => {
+
+                const { id, title, content, image, tags } = post;
+
+                return (
+
+                  <tr key={id}>
+                    <td><p>{title}</p></td>
+                    <td><p>{content}</p></td>
+                    <td>
+                      <figure>
+                        <img src={image} alt={title} />
+                      </figure>
+                    </td>
+                    <td><p>{tags}</p></td>
+                  </tr>
+
+                )
+              } )
+            }
+          </tbody>
+        </table>
       </div>
     </>
 
