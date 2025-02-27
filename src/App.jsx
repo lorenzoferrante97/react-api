47c217ec6ff497ce9bc17a1ef708eb35789e4fe2
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-27 15:00:10 +0100

    add: onchange function

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
 
   const [ posts, setPosts ] = useState([]);
 
+
+  // GET -> index
   const getPosts = () => {
 
     fetch('http://localhost:3000/posts/')
@@ -14,9 +16,10 @@ function App() {
 
   }
 
+  // mostra elenco posts al montaggio comp
   useEffect( () => { getPosts(); }, [] )
 
-  // elimina post
+  // DELETE -> destroy
   const deletePost = (id) => {
 
     fetch(`http://localhost:3000/posts/${id}`, { method: 'DELETE' })
@@ -25,10 +28,58 @@ function App() {
 
   }
 
+  // var dinamica form values
+  const [ formValues, setFormValues ] = useState({});
+
+  const handleField = (e) => {
+      const { name, value } = e.target;
+      const updateFormValues = {
+        ...formValues,
+        [name]: value
+      }
+      setFormValues ( updateFormValues )
+  }
+
   return (
 
     <>
-      <div className="w-full max-h-screen p-10u overflow-y-auto">
+      <div className="w-full max-h-screen p-10u overflow-y-auto flex flex-col gap-7u">
+
+        <div className='w-full flex flex-col gap-5u'>
+
+          <h1 className='font-h1'>Crea un post</h1>
+
+          {/* form aggiunta post */}
+          <form className='flex flex-col gap-3u max-w-[480px]'>
+            {/* post title */}
+            <div className='flex flex-col gap-u'>
+              <label htmlFor="postTitle">Titolo post</label>
+              <input type="text" placeholder='Carbonara' name='postTitle' className='border border-smoke-100 rounded-md px-2u py-u' onChange={ handleField } />
+            </div>
+            {/* post content */}
+            <div className='flex flex-col gap-u'>
+              <label htmlFor="postContent">Contenuto post</label>
+              <textarea type="text" placeholder='Scrivi qui il contenuto del tuo post' name='postContent' className='border border-smoke-100 rounded-md px-2u py-u' onChange={ handleField } />
+            </div>
+            {/* post img */}
+            <div className='flex flex-col gap-u'>
+              <label htmlFor="postImg">URL immagine</label>
+              <input type="text" placeholder='link immagine' name='postImg' className='border border-smoke-100 rounded-md px-2u py-u' onChange={ handleField } />
+            </div>
+            {/* post tags */}
+            <div className='flex flex-col gap-u'>
+              <label htmlFor="postTags">Tags post</label>
+              <input type="text" placeholder='ricetta, primo, spaghetti...' name='postTags' className='border border-smoke-100 rounded-md px-2u py-u' onChange={ handleField } />
+            </div>
+            {/* submit button */}
+            <div className='flex flex-col gap-u'>
+              <button type='submit' className='px-7u py-4u w-full bg-black text-white rounded-md md:w-fit'>Inserisci post</button>
+            </div>
+          </form>
+          
+        </div>
+
+
         {/* tabella */}
         <table className='bg-smoke-50 w-full h-full overflow-y-scroll'>
           <thead>
